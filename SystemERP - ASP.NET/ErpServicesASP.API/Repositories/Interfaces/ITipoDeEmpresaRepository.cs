cc33bd31c6cfaf4a6320bc9b8619c13303dfd366
khaleusanches <khaleusanches@gmail.com>
2025-02-15 15:42:31 -0300

    feat: Tipos De Empresas

---
+++ SystemERP - ASP.NET/ErpServicesASP.API/Repositories/Interfaces/ITipoDeEmpresaRepository.cs
@@ -0,0 +1,15 @@
+﻿using ErpServicesASP.API.Dto;
+using ErpServicesASP.API.Model;
+
+namespace ErpServicesASP.API.Repositories.Interfaces
+{
+    public interface ITipoDeEmpresaRepository
+    {
+        Task<TipoDeEmpresaModel> GetTipoDeEmpresaPorID(int id);
+        Task<List<TipoDeEmpresaModel>> ListarTiposDeEmpresa();
+        Task<TipoDeEmpresaModel> CriarTipoDeEmpresa(TipoDeEmpresaCreateDto novoTipoDeEmpresa);
+        Task<List<TipoDeEmpresaModel>> DeletarTipoDeEmpresa(TipoDeEmpresaModel tipoDeEmpresa);
+        Task<TipoDeEmpresaModel> AtualizarTipoDeEmpresa(TipoDeEmpresaModel tipoDeEmpresa);
+        Task<bool> tipoJaExiste(string nomeTipoDeEmpresa);
+    }
+}
---
+++ SystemERP - ASP.NET/ErpServicesASP.API/Services/Interfaces/ITipoDeEmpresaService.cs
@@ -0,0 +1,14 @@
+﻿using ErpServicesASP.API.Dto;
+using ErpServicesASP.API.Model;
+
+namespace ErpServicesASP.API.Services.Interfaces
+{
+    public interface ITipoDeEmpresaService
+    {
+        Task<ResponseModel<TipoDeEmpresaModel>> GetTipoDeEmpresaPorID(int id);
+        Task<ResponseModel<List<TipoDeEmpresaModel>>> ListarTiposDeEmpresa();
+        Task<ResponseModel<TipoDeEmpresaModel>> CriarTipoDeEmpresa(TipoDeEmpresaCreateDto novoTipoDeEmpresa);
+        Task<ResponseModel<List<TipoDeEmpresaModel>>> DeletarTipoDeEmpresa(TipoDeEmpresaModel tipoDeEmpresa);
+        Task<ResponseModel<TipoDeEmpresaModel>> AtualizarTipoDeEmpresa(TipoDeEmpresaModel tipoDeEmpresa);
+    }
+}
